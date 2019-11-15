5704f97cf89581e6618001ad104a097ac715d6da
Brett Meehan <bmeehan@atlassian.com>
2019-11-15 19:35:53 +1100

    ITOPSENG-101-2 more dynamic testing using pytest fixture

--- roles/product_install/molecule/jira_all/tests/test_default.py
+++ roles/product_install/molecule/jira_all/tests/test_default.py
@@ -1,31 +1,36 @@
 import os
 from six.moves import urllib
-
+import pytest
 import testinfra.utils.ansible_runner
 import json
+from pprint import pprint
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+@pytest.fixture
+def ansiblevars(host):
+    ansiblevars = json.loads(host.file('/tmp/ansible.vars').content_string)
+    return ansiblevars
 
-def test_version_is_correct(host):
-    verfile = host.file('/media/atl/jira/shared/jira-software.version')
+def test_version_is_correct(host, ansiblevars):
+    verfile = host.file(ansiblevars['atl_product_version_file']['cmd'][1])
     assert verfile.exists
+    assert verfile.content.decode("UTF-8").strip() == ansiblevars['atl_product_version']
 
-    assert verfile.content.decode("UTF-8").strip() == "7.13.1"
-
-def test_is_downloaded(host):
-    installer = host.file('/media/atl/downloads/jira-software.7.13.1-x64.bin')
+def test_is_downloaded(host, ansiblevars):
+    installer = host.file(ansiblevars['atl_product_download'])
     assert installer.exists
     assert installer.user == 'root'
 
-def test_completed_lockfile(host):
-    lockfile = host.file('/media/atl/downloads/jira-software.7.13.1-x64.bin_completed')
+def test_completed_lockfile(host, ansiblevars):
+    ansiblevars = json.loads(host.file('/tmp/ansible.vars').content_string)
+    lockfile = host.file(ansiblevars['atl_product_download']+'_completed')
     assert lockfile.exists
     assert lockfile.user == 'root'
 
-def test_is_unpacked(host):
-    installer = host.file('/opt/atlassian/jira-software/7.13.1/atlassian-jira/')
+def test_is_unpacked(host, ansiblevars):
+    installer = host.file(ansiblevars['atl_installation_base']+'/'+ansiblevars['atl_product_edition']+'/'+ansiblevars['atl_product_version']+'/atlassian-jira/')
     assert installer.exists
     assert installer.is_directory
     assert installer.user == 'jira'
