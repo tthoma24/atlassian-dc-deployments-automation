3e3d83d162fffc3812f4f75b7569260d4fcfc220
dbacon <dbacon@atlassian.com>
2020-05-20 14:14:05 +0100

    AZURE-211 SQL Server support for Azure Quick Start

--- roles/az_app_insights_install/molecule/default/tests/test_default.py
+++ roles/az_app_insights_install/molecule/default/tests/test_default.py
@@ -20,19 +20,19 @@ def test_jaxb_installed(host):
     f = host.file('/usr/share/collectd/java/jaxb-api-2.3.1.jar')
     assert f.exists
 
-# @pytest.mark.parametrize('filename', [
-#   '/opt/atlassian/crowd/current/crowd-webapp/WEB-INF/lib/applicationinsights-core-2.3.1.jar',
-#   '/opt/atlassian/crowd/current/crowd-webapp/WEB-INF/lib/applicationinsights-web-2.3.1.jar',
-#   '/opt/atlassian/crowd/current/crowd-webapp/WEB-INF/lib/applicationinsights-collectd-2.3.1.jar'
-# ])
-# def test_app_insight_jars_downloaded(host, filename):
-#     f = host.file(filename)
-#     assert f.exists
-
-def test_app_insights_collectd_file(host):
-    f = host.file('/usr/share/collectd/java/applicationinsights-collectd-2.3.1.jar')
+@pytest.mark.parametrize('filename', [
+  '/opt/atlassian/crowd/current/crowd-webapp/WEB-INF/lib/applicationinsights-core-2.6.0.jar',
+  '/opt/atlassian/crowd/current/crowd-webapp/WEB-INF/lib/applicationinsights-web-2.6.0.jar',
+  '/opt/atlassian/crowd/current/crowd-webapp/WEB-INF/lib/applicationinsights-agent-2.6.0.jar'
+])
+def test_app_insight_jars_downloaded(host, filename):
+    f = host.file(filename)
     assert f.exists
 
-# def test_applicationinsights_xml_installed(host):
-#     f = host.file('/opt/atlassian/crowd/current/crowd-webapp/WEB-INF/classes/ApplicationInsights.xml')
-#     assert f.exists
+def test_app_insights_collectd_file(host):
+    f = host.file('/usr/share/collectd/java/applicationinsights-collectd-2.6.0.jar')
+    assert f.exists
+
+def test_applicationinsights_xml_installed(host):
+    f = host.file('/opt/atlassian/crowd/current/crowd-webapp/WEB-INF/classes/ApplicationInsights.xml')
+    assert f.exists
